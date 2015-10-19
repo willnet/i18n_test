8d0219388561c6613ffc5548d5aa05be825a44ef
willnet <netwillnet@gmail.com>
2015-10-19 22:55:14 +0900

    Add devise and function of authentication

--- test/integration/translator_app_test.rb
+++ test/integration/translator_app_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class TranslatorAppTest < ActionDispatch::IntegrationTest
-  setup { Translator.reload! }
+  setup do
+    Translator.reload!
+    sign_in(admin)
+  end
+
+  def admin
+    @admin ||= Admin.create!(
+      email: "admin+#{Admin.count}@example.org",
+      password: '12345678'
+    )
+  end
+
+  def sign_in(admin)
+    visit '/admins/sign_in'
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
+    click_button 'Log in'
+  end
 
   test 'can translate messages from a given locale to another' do
     assert_raise I18n::MissingTranslationData do
